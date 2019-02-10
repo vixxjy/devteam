39280e6cc3ef2ddfc601ebebac68b6cea1833004
smilezdev <vixxjy@gmail.com>
2019-02-10 12:08:14 +0000

    http updates

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-if (env('APP_ENV') === 'production') {
-    URL::forceSchema('https');
-}
 
 Route::get('/', [ 'uses' => 'HomeController@index', 'as' => 'landing']);
 Route::get('/aboutus', [ 'uses' => 'PagesController@aboutUs', 'as' => 'about']);
