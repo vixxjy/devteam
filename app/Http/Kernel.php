82d89659cfa62b8a3e57ef1af031fdc582965559
smilezdev <vixxjy@gmail.com>
2019-02-10 13:03:25 +0000

    hpps growth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,9 +19,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-         // appending custom middleware 
-        // 'App\Http\Middleware\HttpsProtocol' 
-        \App\Http\Middleware\HttpsProtocol::class
     ];
 
     /**
@@ -63,7 +60,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        // 'forceSsl' => App\Http\Middleware\HttpsProtocol::class,
     ];
 
     /**
